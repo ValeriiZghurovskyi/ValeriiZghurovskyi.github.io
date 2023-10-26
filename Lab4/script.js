51b9a0a19843a8d2474ad2a9642b101e05c28608
Valerii Zghurovskyi <valerazgur@gmail.com>
2023-10-26 12:34:49 +0300

    Lab4

---
+++ Lab4/script.js
@@ -0,0 +1,23 @@
+const buttonElem = document.querySelector('.readmore-button');
+const modalElem = document.querySelector('.modal');
+
+modalElem.style.cssText = 'display: flex; visibility:hidden; opacity:0; transition: opacity 300ms ease-in-out;';
+
+const closeModal = event => {
+    const target = event.target;
+
+    if (target === modalElem || target.closest('.modal-close')) {
+        modalElem.style.opacity = 0;
+        setTimeout(() => {
+            modalElem.style.visibility = 'hidden';
+        }, 300)
+    }
+}
+
+const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+}
+
+buttonElem.addEventListener('click', openModal);
+modalElem.addEventListener('click', closeModal)
